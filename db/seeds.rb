b7a75198e24be0375bb3918ba7c5b209b79a21b1
Obafemi Fadairo <obafad94@gmail.com>
2018-11-23 14:07:23 -0500

    Make proper db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,12 @@ funmi = User.create(username: "funmi", password: "password")
 
 # shouldn't be able to give more than one upvote
 # i guess i can create an array that stores the ones that someone has commented on?
+# if u upvote you shouldn't be able to downvote
 
 post1 = Post.create(title: "Kanye", content: "is so cool", user_id: 1, upvotes: 1, downvotes: 0)
 post2 = Post.create(title: "Grafitti", content: "Has a rich history", user_id: 2, upvotes: 3, downvotes: 0)
 
 
 comment1 = Comment.create(content: "I concur!", post_id: 1, user_id: 3, upvotes: 1, downvotes: 0)
-comment2 = Comment.create(title: "Kanye", content: "is so cool", user_id: 1, upvotes: 1, downvotes: 0)
-comment3 = Comment.create(title: "Kanye", content: "is so cool", user_id: 1, upvotes: 1, downvotes: 0)
+comment2 = Comment.create(content: "IDK, I really like Kendrick Lamar", post_id: 1, user_id: 2, upvotes: 1, downvotes: 0)
+comment3 = Comment.create(content: "Just like the romans!", post_id: 2, user_id: 4, upvotes: 3, downvotes: 0)
