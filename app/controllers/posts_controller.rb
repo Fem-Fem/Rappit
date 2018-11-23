1b60eeb45d463028ca55d203d33b5459c59271c9
Obafemi Fadairo <obafad94@gmail.com>
2018-11-23 12:55:47 -0500

    Work on posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ActionController::Base
   def create
     @post = Post.new(post_params)
     if @post.save
-    	post_path(@post)
+    	redirect_to post_path(@post)
     else
       @errors = @post.errors
       erb :'/new'
@@ -14,7 +14,21 @@ class PostsController < ActionController::Base
   end
 
   def show
-    @user = Post.find(params[:id])
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update!(post_params)
+      #redirect to comment
+    else
+      @errors = @post.errors
+      erb :'/edit'
+    end
   end
 
   def index
@@ -24,7 +38,7 @@ class PostsController < ActionController::Base
   private
 
   def post_params
-    params.require(:user).permit(:title, :content)
+    params.require(:post).permit(:title, :content)
   end
 
 end
