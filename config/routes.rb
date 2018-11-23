d87f9cbb7cc3bb928ca1ca6257f99cb69432e3b8
Obafemi Fadairo <obafad94@gmail.com>
2018-11-23 13:06:55 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :posts, only: [:index, :show, :create, :edit, :update, :delete, :new] do
+    	resources :comments, only: [:index, :show, :create, :edit, :update, :delete, :new]
+	end
+	# login?
 end
