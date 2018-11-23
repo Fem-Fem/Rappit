fd52422f6778facc3b979b8a2e1c7b8aff532aea
Obafemi Fadairo <obafad94@gmail.com>
2018-11-23 13:49:22 -0500

    Fix password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :posts
 	has_many :comments
+	has_secure_password
 	validates :username, :password, presence: true
   	validates :username, uniqueness: true
   end
