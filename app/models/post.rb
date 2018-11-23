97ff4464a631f59d29de58e913d5c465607fe442
Obafemi Fadairo <obafad94@gmail.com>
2018-11-23 12:47:32 -0500

    Work on models

---
+++ app/models/post.rb
@@ -0,0 +1,19 @@
+class Post < ActiveRecord::Base
+	has_many :comments
+	belongs_to :user
+	validates :title, :content, presence: true
+
+	def order_by_rating
+		Post.order(rating: asc_or_desc)
+		asc_or_desc
+	end
+
+	def order_by_newness(asc_or_desc)
+		Post.order(date: ?)
+		asc_or_desc
+	end
+
+	def order_by_category
+		Post.where(c)
+	end
+end
