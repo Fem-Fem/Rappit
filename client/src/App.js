ee2532aa64fd21360f0f7ec65f146953261e75bf
Obafemi Fadairo <obafad94@gmail.com>
2018-12-13 20:33:44 -0500

    Working on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-            Learn React
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/posts" component={Posts} exact/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
