a3b88283f9dc25f8eb5113ddebcd0bfc02cc542f
Obafemi Fadairo <obafad94@gmail.com>
2018-12-13 21:37:13 -0500

    Work on comment form

---
+++ client/src/components/comments/createComment.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class createComment extends Component {
+	render(
+		return(
+			<div>
+				<form>
+					<p>
+						<label>Content</label>
+						<input type="text" />
+					</p>
+					<input type="submit" />
+				</form>
+			<div>
+		)
+	)
+}
+
+export default Home;
