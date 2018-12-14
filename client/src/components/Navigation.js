08d32cb4e05877ad34df5d8541733084831872ce
Obafemi Fadairo <obafad94@gmail.com>
2018-12-13 21:17:08 -0500

    Working on navigation.js

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+const Navigation = () => {
+	return(
+		<div>
+			<NavLink to="/posts">All Posts</NavLink>
+			<NavLink to="/posts/1">Your Posts</NavLink>
+		</div>
+	)
+}
+
+export default Navigation;
