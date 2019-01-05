44faac90c53b9c864773f259aa12bb4798ea68df
Obafemi Fadairo <obafad94@gmail.com>
2019-01-05 15:52:00 -0500

    Working on login/signup forms

---
+++ client/src/components/login/LoginForm.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Button, FormGroup, FormControl, ControlLabel} from "react-bootstrap"
+
+export default class LoginForm extends React.component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	this.state = {
+		username: "",
+		password: "",
+	};
+}
+
+validateForm() {
+	return this.state.username.length > 0 && this.sate.password.length > 0;
+}
+
+handleChange = event => {
+	this.setState({
+		[event.target.id]: event.target.value
+	});
+}
+
+handleSubmit = event => {
+	event.preventDefault();
+}
+
+render() {
+	return (
+		<form onSubmit = {this.handleSubmit}>
+			<FormGroup>
+				<ControlLabel>Username</ControlLabel>
+				<FormControl
+				type="text"
+				value={this.state.value}
+				placeholder="Enter username"
+				onChange={this.handleChange}
+				/>
+			</FormGroup>
+			<FormGroup controlID="password">
+				<ControlLabel>Password</ControlLabel>
+				<FormControl
+				value={this.state.value}
+				placeholder="Enter password"
+				onChange={this.handleChange}
+				/>
+			</FormGroup>
+			<Button type="submit">Login</Button>
+		</form>
+	)
+}
---
+++ client/src/components/login/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+
+class LoginPage extends React.Compoennt {
+	render() {
+		return (
+			<div>
+				<LoginForm />
+			</div>
+		)
+	}
+}
+
+export default LoginPage
