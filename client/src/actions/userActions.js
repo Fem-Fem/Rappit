39af48ff24836b06886b17d09384bc00ab1aef51
Obafemi Fadairo <obafad94@gmail.com>
2019-01-11 19:33:26 -0500

    Work on actions, reducers folders

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,1 +1,28 @@
 const baseUrl = 'http://localhost:3000'
+
+export const loginUser = () => {
+	fetch(`${baseUrl}/login`)
+	.then(response => response.json())
+	.then(posts =>dispatch({
+		type: 'STORE_USER',
+	}))
+	.catch(err => err)
+}
+
+export const signupUser = () => {
+	fetch(`${baseUrl}/signup`)
+	.then(response => response.json())
+	.then(posts =>dispatch({
+		type: 'STORE_USER',
+	}))
+	.catch(err => err)
+}
+
+export const logoutUser = () => {
+	fetch(`${baseUrl}/logout`)
+	.then(response => response.json())
+	.then(posts =>dispatch({
+		type: 'UNSTORE_USER',
+	}))
+	.catch(err => err)
+}
