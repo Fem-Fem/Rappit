106e2d7c53abddecad48a777e24bbe085ffb9e3f
Obafemi Fadairo <obafad94@gmail.com>
2019-01-12 16:48:53 -0500

    Work on posts container

--- client/src/containers/postsContainer.js
+++ client/src/containers/postsContainer.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-class PostsContainer extends Component {
+import { fetchPosts, createPost, deletePost } from '../actions/userActions'
+import createPost from '../components/posts/createPost'
+
+class postsContainer extends Component {
+	componentWillMount() {
+		this.props.fetchPosts()
+	}
 
 	render() {
+
+		const posts = this.props
 		return(
-			<div></div>
+			<div>
+				<createPost/>
+				{posts.map(post => <Post id= {post.id} post={post} deletePost={this.props.deletePost}/>)}
+			</div>
 		)
 	}
-};
+}
+
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+	fetchPosts,
+	createPost,
+	deletePost}
+}, dispatch)
+
+export default connect(null, mapDispatchToProps)(postsContainer)
