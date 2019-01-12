c0cff319f880058299ce0ba1b8101455baa1403b
Obafemi Fadairo <obafad94@gmail.com>
2019-01-11 19:29:02 -0500

    Work on actions, reducers folders

--- client/src/actions/commentActions.js
+++ client/src/actions/commentActions.js
@@ -1,1 +1,10 @@
 const baseUrl = 'http://localhost:3000'
+
+export const createComment = () => {
+	fetch(`${baseUrl}/comments`)
+	.then(response => response.json())
+	.then(comments =>dispatch({
+		type: 'ADD_COMMENT',
+	}))
+	.catch(err => err)
+}
--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,11 +1,19 @@
 const baseUrl = 'http://localhost:3000'
 
-
 export const fetchPosts = () => {
 	fetch(`${baseUrl}/posts`)
 	.then(response => response.json())
 	.then(posts =>dispatch({
 		type: 'GET_POSTS',
+	}))
+	.catch(err => err)
+}
+
+export const createPost = () => {
+	fetch(`${baseUrl}/posts`)
+	.then(response => response.json())
+	.then(posts =>dispatch({
+		type: 'ADD_POST',
 	}))
 	.catch(err => err)
 }
