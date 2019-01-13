128905a15298c77989aaf60ab0099ac017f461ce
Obafemi Fadairo <obafad94@gmail.com>
2019-01-12 21:16:42 -0500

    Work on comment and post containers

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import commentsContainer from '../containers/commentsContainer';
+
 
 const Post = ({post: {id, title, content}, deletePost}) => {
 	return (
 		<div>
 			<li>
-				{title}
+				<Link to="/login">{title}</Link>
+				<Route path="/{title}" component={commentsContainer}/>
 				<button onClick={() => deletePost(id)}>Delete</button>
 			</li>
 		</div>
---
+++ client/src/containers/commentsContainer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { fetchComments, createComment, deleteComment } from '../actions/commentActions'
+import CommentForm from '../components/comments/CommentForm'
+import Comment from '../components/comments/Comment'
+
+class commentsContainer extends Component {
+	componentWillMount() {
+		this.props.fetchComments()
+	}
+
+	render() {
+
+		const { posts } = this.props
+		return(
+			<div>
+				<CommentForm/>
+				{posts.map(post => <Post id= {post.id} post={post} deleteComment={this.props.deleteComment}/>)}
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		user: state.user.logged_in,
+		posts: state.posts.posts
+	}
+}
+
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+	fetchComments,
+	createComment,
+	deleteComment
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(commentsContainer)
