ed35c652dc17843938ab20e3773a9fea9b82f8f5
Obafemi Fadairo <obafad94@gmail.com>
2019-01-18 03:42:38 -0500

    add some user info to api

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :content, :user_id
+  belongs_to :user, serializer: UserSerializer
   belongs_to :post
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :category, :user_id
+  belongs_to :user, serializer: UserSerializer
   has_many :comments
 end
