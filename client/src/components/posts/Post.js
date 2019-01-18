f5278f919c29f7a44aff7cf4ad3ef8f49df1eca5
Obafemi Fadairo <obafad94@gmail.com>
2019-01-18 03:19:57 -0500

    fix unnecessary key issue

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -3,10 +3,10 @@ import commentsContainer from '../../containers/commentsContainer';
 import { Link} from "react-router-dom";
 import '../../post.css'
 
-const Post = ({post: {id, title, content, user_id}, deletePost, fetchPost, key}) => {
+const Post = ({post: {id, title, content, user_id}, deletePost, fetchPost}) => {
 	return (
 		<div>
-			<div id="post" className={key}>
+			<div id="post" className={id}>
 				<Link to={`/posts/${id}`} onClick={() => fetchPost(id)}>{title}</Link> - {content}
 			</div>
 			<button onClick={() => deletePost(id, user_id)}>Delete Post</button>
