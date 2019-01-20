f521f24ad41d544bc524d8bb41fe8a0b78d6c6c8
Obafemi Fadairo <obafad94@gmail.com>
2019-01-19 23:29:17 -0500

    remove unnecessary import

--- client/src/containers/postsContainer.js
+++ client/src/containers/postsContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Link } from "react-router-dom";
 
 import { fetchPosts, createPost, deletePost, fetchPost } from '../actions/postActions'
 
