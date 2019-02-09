6de4bd2a578be2f745e3ebcf18ae86727a6e3562
Obafemi Fadairo <obafad94@gmail.com>
2019-02-09 03:00:54 -0500

    change db to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,20 +12,23 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
-  create_table "comments", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", id: :serial, force: :cascade do |t|
     t.integer "user_id"
     t.string "content"
     t.integer "post_id"
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", id: :serial, force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
     t.string "content"
     t.string "category"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
   end
